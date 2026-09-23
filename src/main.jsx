8b14eef2eef895abaab00c95017df3d6f1bf2f6a
Sourabhsingh88 <Sourabhsinghmandloi122@gmail.com>
2026-09-23 21:02:35 +0530

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import './styles/globals.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <BrowserRouter basename="/jusstCode">
         <App />
         <Toaster
           position="top-right"
