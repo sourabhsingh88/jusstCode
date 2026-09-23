83e92249366ea02a0663a80e5543a4472b239602
Sourabhsingh88 <Sourabhsinghmandloi122@gmail.com>
2026-09-23 20:40:43 +0530

    sg

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:5000',
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks: {
-//           vendor: ['react', 'react-dom'],
-//           three: ['three', '@react-three/fiber', '@react-three/drei'],
-//           motion: ['framer-motion'],
-//         },
-//       },
-//     },
-//   },
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-   base: '/jusstCode/',
   server: {
     port: 5173,
-    allowedHosts: ['encroach-oxidant-dusk.ngrok-free.dev'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -54,3 +24,34 @@ export default defineConfig({
     },
   },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+
+//   server: {
+//     port: 5173,
+//     allowedHosts: ['encroach-oxidant-dusk.ngrok-free.dev'],
+
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom'],
+//           three: ['three', '@react-three/fiber', '@react-three/drei'],
+//           motion: ['framer-motion'],
+//         },
+//       },
+//     },
+//   },
+// })
